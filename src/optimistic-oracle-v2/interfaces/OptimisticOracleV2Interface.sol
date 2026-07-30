43d1573e12d619167eab39973261082df20a75ab
Pablo Maldonado <pablo@umaproject.org>
2026-07-30 11:16:43 +0100

    fix: uppercase ancillary bytes limit constant

--- src/optimistic-oracle-v2/interfaces/OptimisticOracleV2Interface.sol
+++ src/optimistic-oracle-v2/interfaces/OptimisticOracleV2Interface.sol
@@ -127,7 +127,7 @@ abstract contract OptimisticOracleV2Interface {
     // This value must be <= the Voting contract's `ancillaryBytesLimit` value otherwise it is possible
     // that a price can be requested to this contract successfully, but cannot be disputed because the DVM refuses
     // to accept a price request made with ancillary data length over a certain size.
-    uint256 public constant ancillaryBytesLimit = 8192;
+    uint256 public constant ANCILLARY_BYTES_LIMIT = 8192;
 
     function defaultLiveness() external view virtual returns (uint256);
 
