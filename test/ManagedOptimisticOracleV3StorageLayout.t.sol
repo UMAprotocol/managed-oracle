484ad9a69aa6b3f83e6a5afca3a86211625a0d37
Pablo Maldonado <pablo@umaproject.org>
2026-08-14 17:44:14 +0100

    ci: run audited bytecode checks on macOS

--- test/ManagedOptimisticOracleV3StorageLayout.t.sol
+++ test/ManagedOptimisticOracleV3StorageLayout.t.sol
@@ -5,21 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {Upgrades} from "@openzeppelin/foundry-upgrades/Upgrades.sol";
 import {Options} from "@openzeppelin/foundry-upgrades/Options.sol";
 
-import {ManagedOptimisticOracleV3} from "../src/optimistic-oracle-v2/implementation/ManagedOptimisticOracleV3.sol";
-import {ManagedOptimisticOracleV3ProductionUpgradeChecks} from
-    "../script/production/ManagedOptimisticOracleV3ProductionUpgradeChecks.sol";
-
-contract ManagedOptimisticOracleV3StorageLayoutTest is Test, ManagedOptimisticOracleV3ProductionUpgradeChecks {
-    function testReviewedTargetBytecode() public {
-        assertEq(
-            keccak256(type(ManagedOptimisticOracleV3).creationCode),
-            TARGET_CREATION_CODEHASH,
-            "target creation bytecode drift"
-        );
-
-        _assertTargetImplementation(address(new ManagedOptimisticOracleV3()));
-    }
-
+contract ManagedOptimisticOracleV3StorageLayoutTest is Test {
     function testStorageLayoutIsCompatibleWithProductionV2() public {
         Options memory opts;
         opts.referenceContract = "build-info-prod-current:ManagedOptimisticOracleV2";
